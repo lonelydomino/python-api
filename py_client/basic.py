5aaf5612acd3fd2dee8b42eaab47f9860c148563
lonelydomino <lonelydomino@icloud.com>
2023-11-17 19:51:32 -0800

    Adding API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,8 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
 
-print(get_response.text)
+print(get_response.json()['message'])
 
